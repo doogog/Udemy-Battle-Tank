4d739230e8ad6caad84139ffa9252132ff5b7e93
DooGoG <doogog@playsaurus.com>
2018-09-12 03:25:12 -0700

    no message

---
+++ Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG( LogTemp, Warning, TEXT("PlayerController Begin Play!") );
+
+	ATank* ControlledTank = GetControlledTank();
+	if ( ControlledTank )
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController controlled: %s"), *ControlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController has no possessed Tank!"));
+	}
+}
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
